ecad84af362feac2b88d11b99878a790079ef585
Adam Spiers <lilypond@adamspiers.org>
2013-01-08 00:37:21 +0000

    add GPLv3 header and full copy (issue #10)
    https://github.com/aspiers/ly2video/issues/10

--- ly2video.py
+++ ly2video.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+# ly2video - generate performances video from LilyPond source files
+# Copyright (C) 2012 Jiri "FireTight" Szabo
+# Copyright (C) 2012 Adam Spiers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information about this program, please visit
+# <https://github.com/aspiers/ly2video/>.
+
 import collections
 import os
 import re
