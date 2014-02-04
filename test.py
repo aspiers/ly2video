8a9880fb35404bf51dc735488304a1e2e98628f5
Emmanuel Leguy <manu@algomus.fr>
2014-02-05 00:42:59 +0200

    unit tests: 'TimeCode' class testing
    Add tests for the new class TimeCode, update some tests.
    Some new fonctional tests (testCompleteFollowing)
    
    Tested classes:
    	* TimeCode:
    		. ticksToSecs()
    		. secsElapsedForTempoChanges()
    		. nbFramesToNextNote()
    		. goToNextNote()
    		. atEnd()
    	* ScoreImage:
    		. currentXposition
    		. moveToNextNote()
    		. cropFrame()

--- test.py
+++ test.py
@@ -24,8 +24,128 @@
 
 import unittest
 from video import *
+from synchro import *
 from PIL import Image
 
+class TimeCodeTest (unittest.TestCase):
+    
+    def setUp(self):
+        self.timecode = TimeCode([0,384,768,1152],[(0,60.0)], 384, 30.0)
+    
+    def testTicksToSecs_withNullInterval (self):
+        for tempo in range (1,300):
+            self.timecode.tempo = float(tempo)
+            secsSinceStartIndex = self.timecode.ticksToSecs(0, 0)
+            self.assertEqual(secsSinceStartIndex, 0.0, "")
+    
+    def testTicksToSecs_withOneSecInterval (self):
+        secsSinceStartIndex = self.timecode.ticksToSecs(0, 384)
+        self.assertEqual(secsSinceStartIndex, 1.0, "")
+        
+    def testTicksToSecs_withTwoSecInterval (self):
+        secsSinceStartIndex = self.timecode.ticksToSecs(0, 768)
+        self.assertEqual(secsSinceStartIndex, 2.0, "")
+
+    def testTicksToSecs_withOtherTempo (self):
+        self.timecode.tempo = 90.0
+        secsSinceStartIndex = self.timecode.ticksToSecs(0, 1152)
+        self.assertEqual(secsSinceStartIndex, 2.0, "")
+        
+    def testSecsElapsedForTempoChanges (self):
+        self.timecode.temposList = [(0, 60.0),(1152, 90.0),(3456, 60.0)]
+        result = self.timecode.secsElapsedForTempoChanges(startTick = 0, endTick = 1152)
+        self.assertEqual(result,3.0,"")
+        result = self.timecode.secsElapsedForTempoChanges(startTick = 0, endTick = 3456)
+        self.assertEqual(result,6.0,"")
+        result = self.timecode.secsElapsedForTempoChanges(startTick = 1152, endTick = 3456)
+        self.assertEqual(result,4.0,"")
+        result = self.timecode.secsElapsedForTempoChanges(startTick = 3456, endTick = 4608)
+        self.assertEqual(result,3.0,"")
+        result = self.timecode.secsElapsedForTempoChanges(startTick = 0, endTick = 4608)
+        self.assertEqual(result,12.0,"")
+        
+    def testNbFramesToNextNote(self):
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 30, "")
+
+    def testNbFramesToNextNote_withApprox_plus(self):
+        self.timecode = TimeCode([0,380,768,1152],[(0,60.0)], 384, 30.0)
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 30, "")
+
+    def testNbFramesToNextNote_withApprox_minus(self):
+        self.timecode = TimeCode([0,390,768,1152],[(0,60.0)], 384, 30.0)
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 30, "")
+        
+    def testNbFramesToNextNote_withApprox_correction(self):
+        self.timecode = TimeCode([0,370,768,1152],[(0,60.0)], 384, 30.0)
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 29, "")
+        self.timecode.goToNextNote()
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 31, "")
+
+    def testNbFramesToNextNote_withTempoChange(self):
+        self.timecode = TimeCode([0,384,768,1152],[(0,60.0),(384,90.0),(768,60.0)], 384, 30.0)
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 30, "")
+        self.timecode.goToNextNote()
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 20, "")
+        self.timecode.goToNextNote()
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 30, "")
+
+    def testNbFramesToNextNote_withTempoChange_withApproxCorrection(self):
+        self.timecode = TimeCode([0,370,768,1152],[(0,60.0),(370,90.0),(768,60.0)], 384, 30.0)
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 29, "")
+        self.timecode.goToNextNote()
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 21, "")
+        self.timecode.goToNextNote()
+        self.assertEqual(self.timecode.nbFramesToNextNote(), 30, "")
+
+    def testGotToNextNote (self):
+        self.assertEqual(self.timecode.currentOffset, 0, "")
+        self.assertEqual(self.timecode.nextOffset, 1, "")
+        self.timecode.goToNextNote()
+        self.assertEqual(self.timecode.currentOffset, 1, "")
+        self.assertEqual(self.timecode.nextOffset, 2, "")
+
+    def testGotToNextNote_withScoreImageNotification (self):
+        image = ScoreImage(16,16,Image.new("RGB",(16,16),(0,0,0)), [0,1,2,3], [])
+        self.timecode.registerObserver(image)
+        self.timecode.goToNextNote()
+        self.assertEqual(image.currentXposition,1,"")
+
+    def testGotToNextNote_withSlideShowNotification (self):
+        image = Image.new("RGB",(16,16),(0,0,0))
+        image.save("test0000.0000.png")
+        slideshow = SlideShow("test")
+        self.timecode.registerObserver(slideshow)
+        self.timecode.goToNextNote()
+        self.assertEqual(slideshow.startOffset,1.0,"")
+        os.remove("test0000.0000.png")
+        
+    def testCompleteFollowing (self):
+        scoreimage = ScoreImage(16,16,Image.new("RGB",(16,16),(0,0,0)), [0,1,2,3], [])
+        image = Image.new("RGB",(16,16),(0,0,0))
+        image.save("test0000.0000.png")
+        slideshow = SlideShow("test")
+        self.timecode.registerObserver(scoreimage)
+        self.timecode.registerObserver(slideshow)
+        memPos = scoreimage.currentXposition
+        memOffset = slideshow.startOffset
+        while not self.timecode.atEnd():
+            self.timecode.goToNextNote()
+            # has the scoreImage position changed?
+            self.assertNotEqual(scoreimage.currentXposition, memPos, "")
+            memPos = scoreimage.currentXposition
+            # has the slideshow current offset changed?
+            self.assertNotEqual(slideshow.startOffset, memOffset, "")
+            memOffset = slideshow.startOffset
+        os.remove("test0000.0000.png")
+       
+    def testNotAtEnd(self):
+        self.assertFalse(self.timecode.atEnd(), "")
+
+    def testAtEnd(self):
+        self.timecode.goToNextNote()
+        self.timecode.goToNextNote()
+        self.assertTrue(self.timecode.atEnd(), "")    
+        
 class ScoreImageTest (unittest.TestCase):
     
     def setUp(self):
@@ -131,6 +251,63 @@ class ScoreImageTest (unittest.TestCase):
         self.assertEqual(h, 40, "")
         self.assertEqual(cursorX, 50 , "")
 
+    def test__cropFrame_changeCropping (self):
+        image = Image.new("RGB",(1000,200),(255,255,255))
+        ox=20
+        for x in range(51) : image.putpixel((x+ox,20),(0,0,0))
+        scoreImage = ScoreImage(500,40,image, [], [], 50, 200)
+        areaFrame, cursorX = scoreImage._ScoreImage__cropFrame(index = 300)
+        # must be tequal to left margin
+        self.assertEqual(cursorX, 50 , "")
+        
+    def test__cropFrame_lastCropping (self):
+        image = Image.new("RGB",(1000,200),(255,255,255))
+        ox=20
+        for x in range(51) : image.putpixel((x+ox,20),(0,0,0))
+        scoreImage = ScoreImage(500,40,image, [], [], 50, 200)
+#        # first picture cropping
+#        areaFrame = scoreImage._ScoreImage__cropFrame(index = 100)
+#        self.assertEqual(areaFrame.cursorX, 127 , "")
+#        areaFrame = scoreImage._ScoreImage__cropFrame(index = 200)
+#        self.assertEqual(areaFrame.cursorX, 227 , "")
+#        # second picture cropping
+#        areaFrame = scoreImage._ScoreImage__cropFrame(index = 300)
+#        self.assertEqual(areaFrame.cursorX, 50 , "")
+#        areaFrame = scoreImage._ScoreImage__cropFrame(index = 400)
+#        self.assertEqual(areaFrame.cursorX, 150 , "")
+#        areaFrame = scoreImage._ScoreImage__cropFrame(index = 500)
+#        self.assertEqual(areaFrame.cursorX, 250 , "")
+        # third (last) picture cropping
+        areaFrame, cursorX = scoreImage._ScoreImage__cropFrame(index = 600)
+        self.assertEqual(cursorX, 50 , "")
+        areaFrame, cursorX = scoreImage._ScoreImage__cropFrame(index = 700)
+        self.assertEqual(cursorX, 200 , "")
+        areaFrame, cursorX = scoreImage._ScoreImage__cropFrame(index = 800)
+        self.assertEqual(cursorX, 300 , "")
+        areaFrame, cursorX = scoreImage._ScoreImage__cropFrame(index = 900)
+        self.assertEqual(cursorX, 400 , "")
+        # Must overflow of the right margin for the last cropping.
+        self.assertGreater(cursorX, 300, "")
+        # No new cropping. For a new cropping the cursor position is equals to left margin.
+        self.assertNotEqual(cursorX, 50, "")
+    
+    def test__init__Xposition(self):
+        image = Image.new("RGB",(1000,200),(255,255,255))
+        scoreImage = ScoreImage(500,40,image, [1,2,3], [], 50, 200)
+        self.assertEqual(scoreImage.currentXposition, 1, "")
+        
+    def testMoveToNextNote(self):
+        image = Image.new("RGB",(1000,200),(255,255,255))
+        scoreImage = ScoreImage(500,40,image, [1,2,3], [], 50, 200)
+        scoreImage.moveToNextNote()
+        self.assertEqual(scoreImage.currentXposition, 2, "")
+    
+    def testTravelToNextNote (self):
+        image = Image.new("RGB",(1000,200),(255,255,255))
+        scoreImage = ScoreImage(500,40,image, [10,20,30], [], 50, 200)
+        self.assertEqual(scoreImage.travelToNextNote, 10, "")
+        
+        
     # PUBLIC METHODS
     # topCroppable
     def testTopCroppable_withBlackImage (self):
@@ -212,7 +389,7 @@ class CursorsTest (unittest.TestCase):
     def testWriteMeasureCursorOut (self):
         frame = Image.new("RGB",(16,16),(255,255,255))
         self.assertRaises(Exception, writeMeasureCursor, frame, 20, 30, (255,0,0))
-    
+
 class VideoFrameWriterTest(unittest.TestCase):
 
     def setUp(self):
@@ -228,52 +405,17 @@ class VideoFrameWriterTest(unittest.TestCase):
 
         
     def testPush (self):
-        frameWriter = VideoFrameWriter(30.0,(255,0,0),0,[],[])
+        frameWriter = VideoFrameWriter(30.0,(255,0,0),384.0,[0,384,768,1152],[(0,60.0)])
         frameWriter.scoreImage = Media(1000,200)
         frameWriter.push(Media(100, 100))
         self.assertEqual(frameWriter.height, 300)
     
     def testScoreImageSetter (self):
-        frameWriter = VideoFrameWriter(30.0,(255,0,0),0,[],[])
+        frameWriter = VideoFrameWriter(30.0,(255,0,0),384.0,[0,384,768,1152],[(0,60.0)])
         frameWriter.scoreImage = ScoreImage(1000,200,Image.new("RGB",(1000,200),(255,255,255)), [], [])
         self.assertEqual(frameWriter.width, 1000)
         self.assertEqual(frameWriter.height, 200)
     
-    def testWrite (self):
-        pass
-    
-    def testWriteVideoFrames (self):
-        pass
-        
-    def testTicksToSecs (self):
-        for tempo in range (1,300):
-            self.frameWriter.tempo = float(tempo)
-            secsSinceStartIndex = self.frameWriter.ticksToSecs(0, 0)
-            self.assertEqual(secsSinceStartIndex, 0.0, "")
-        self.frameWriter.tempo = 60.0
-        secsSinceStartIndex = self.frameWriter.ticksToSecs(0, 384)
-        self.assertEqual(secsSinceStartIndex, 1.0, "")
-        secsSinceStartIndex = self.frameWriter.ticksToSecs(0, 768)
-        self.assertEqual(secsSinceStartIndex, 2.0, "")
-        self.frameWriter.tempo = 90.0
-        secsSinceStartIndex = self.frameWriter.ticksToSecs(0, 1152)
-        self.assertEqual(secsSinceStartIndex, 2.0, "")
-        secsSinceStartIndex = self.frameWriter.ticksToSecs(0, 3456)
-        self.assertEqual(secsSinceStartIndex, 6.0, "")
-        
-    def testSecsElapsedForTempoChanges (self):
-        self.frameWriter.temposList = [(0, 60.0),(1152, 90.0),(3456, 60.0)]
-        result = self.frameWriter.secsElapsedForTempoChanges(startTick = 0, endTick = 1152)
-        self.assertEqual(result,3.0,"")
-        result = self.frameWriter.secsElapsedForTempoChanges(startTick = 0, endTick = 3456)
-        self.assertEqual(result,6.0,"")
-        result = self.frameWriter.secsElapsedForTempoChanges(startTick = 1152, endTick = 3456)
-        self.assertEqual(result,4.0,"")
-        result = self.frameWriter.secsElapsedForTempoChanges(startTick = 3456, endTick = 4608)
-        self.assertEqual(result,3.0,"")
-        result = self.frameWriter.secsElapsedForTempoChanges(startTick = 0, endTick = 4608)
-        self.assertEqual(result,12.0,"")
-
     def testFindStaffLinesInImage (self):
         image = Image.new("RGB",(1000,200),(255,255,255))
         for x in range(51) : image.putpixel((x+20,20),(0,0,0))
